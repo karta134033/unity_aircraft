9672402ccecbd8b5c5928ccbc22b18637e04ca7a
Neil <karta134033@gmail.com>
2020-10-02 22:34:42 +0800

    2020/10/02 新增: 1.飛行路徑

---
+++ Assets/Aircraft/Script/AircraftArea.cs
@@ -0,0 +1,11 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aircraft {
+    public class AircraftArea : MonoBehaviour {
+        public CinemachineSmoothPath racePath;
+    }
+
+}
