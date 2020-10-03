830639c1b62c38736905498747dbb8d86645b43a
Neil <karta134033@gmail.com>
2020-10-03 12:50:34 +0800

    2020/10/03 新增: 1.玩家控制飛機模式

--- Assets/Aircraft/Script/AircraftAgent.cs
+++ Assets/Aircraft/Script/AircraftAgent.cs
@@ -2,19 +2,22 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
 
 namespace Aircraft {
-    public class AircraftAgent : MonoBehaviour {
-        public float thrust = 10000f;
+    public class AircraftAgent : Agent {
+        public float thrust = 100000f;
         public float pitchSpeed = 100f;
         public float yawSpeed = 100f;
         public float rollSpeed = 100f;
         public float boostMultiplier = 2f;
         public int NextCheckpointIndex { get; set; }
+
         private AircraftArea area;
         new private Rigidbody rigidbody;
         private TrailRenderer trail;
 
+        // 控制
         private float pitchChange = 0f;
         private float smoothPitchChange = 0f;
         private float maxPitchAngle = 45f;
@@ -24,5 +27,51 @@ namespace Aircraft {
         private float smoothRollChange = 0f;
         private float maxRollAngle = 45f;
         private bool boost;
+
+        public override void Initialize() {
+            area = GetComponentInParent<AircraftArea>();
+            rigidbody = GetComponent<Rigidbody>();
+            trail = GetComponent<TrailRenderer>();
+        }
+
+        public override void OnActionReceived(float[] vectorAction) {
+            pitchChange = vectorAction[0];  // 上
+            if (pitchChange == 2) pitchChange = -1f;  // 下    因為mlagent的關係，所以值會在正數 (0~正數)
+            yawChange = vectorAction[1];  // 右
+            if (yawChange == 2) yawChange = -1f;  // 左
+
+            boost = vectorAction[2] == 1;
+            if (boost && !trail.emitting) trail.Clear();
+            trail.emitting = boost;
+
+            ProcessMovement();
+        }
+
+        private void ProcessMovement() {
+            float boostModifier = boost ? boostMultiplier : 1f;
+            rigidbody.AddForce(transform.forward * thrust * boostModifier, ForceMode.Force);  // 向前的動力
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            float rollAngle = currentRotation.z > 180f ? currentRotation.z - 360f : currentRotation.z;  // 計算roll angle (-180 ~ 180)
+            if (yawChange == 0) {
+                rollChange = -rollAngle / maxRollAngle;
+            }
+            else {
+                rollChange = -yawChange;
+            }
+
+            smoothPitchChange = Mathf.MoveTowards(smoothPitchChange, pitchChange, 2f * Time.fixedDeltaTime);
+            smoothYawChange = Mathf.MoveTowards(smoothYawChange, yawChange, 2f * Time.fixedDeltaTime);
+            smoothRollChange = Mathf.MoveTowards(smoothRollChange, rollChange, 2f * Time.fixedDeltaTime);
+
+            float pitch = currentRotation.x + smoothPitchChange * Time.fixedDeltaTime * pitchSpeed;
+            float yaw = currentRotation.y + smoothYawChange * Time.fixedDeltaTime * yawSpeed;
+            float roll = currentRotation.z + smoothRollChange * Time.fixedDeltaTime * rollSpeed;
+            if (pitch > 180f) pitch -= 360f;
+            pitch = Mathf.Clamp(pitch, -maxPitchAngle, maxPitchAngle);
+            if (roll > 180f) roll -= 360f;
+            roll = Mathf.Clamp(roll, -maxRollAngle, maxRollAngle);
+
+            transform.rotation = Quaternion.Euler(pitch, yaw, roll);
+        }
     }
 }
---
+++ Assets/Aircraft/Script/AircraftPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Aircraft {
+    public class AircraftPlayer : AircraftAgent {
+        [Header("Input Bindings")]
+        public InputAction pitchInput;
+        public InputAction yawInput;
+        public InputAction boostInput;
+        public InputAction pauseInput;
+
+        public override void Initialize() {
+            base.Initialize();
+
+            pitchInput.Enable();
+            yawInput.Enable();
+            boostInput.Enable();
+            pauseInput.Enable();
+        }
+
+        public override void Heuristic(float[] actionsOut) {
+            float pitchValue = Mathf.Round(pitchInput.ReadValue<float>());  // pitch 1 == 上, 0 == none, -1 == 下
+            float yawValue = Mathf.Round(yawInput.ReadValue<float>());  // yaw 1 == 右, 0 == none, -1 == left
+            float boostValue = Mathf.Round(boostInput.ReadValue<float>());  // boost 1 == boost, 0 == none,
+            if (pitchValue == -1f) pitchValue = 2f;
+            if (yawValue == -1f) yawValue = 2f;
+            
+            actionsOut[0] = pitchValue;
+            actionsOut[1] = yawValue;
+            actionsOut[2] = boostValue;
+        }
+
+        private void OnDestory() {  // destory時清理掉所有Input
+            pitchInput.Disable();
+            yawInput.Disable();
+            boostInput.Disable();
+            pauseInput.Disable();
+        }
+    }
+}
